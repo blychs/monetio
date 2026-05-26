8afd424bacd37c752b116c9076f3206ad8eea24f
blychs <plichtig@ucar.edu>
2026-05-26 16:45:28 -0600

    add chimere 3D

--- monetio/models/chimere.py
+++ monetio/models/chimere.py
@@ -1,3 +1,5 @@
+import warnings
+import numpy as np
 import xarray as xr
 
 
@@ -17,7 +19,7 @@ def open_mfdataset(files, var_list=None, surf_only=False, **kwargs):
         Chimere model dataset in standard format for use
         in MELODIES-MONET
     """
-    if not isinstance(files, (list, tuple)):
+    if not isinstance(files, (list, tuple, np.ndarray)):
         files = [files]
 
     datasets = []
@@ -27,6 +29,9 @@ def open_mfdataset(files, var_list=None, surf_only=False, **kwargs):
     # get the data_vars wanted
     if var_list is None:
         var_list = []
+
+    if not surf_only:
+        var_list = var_list + list_met_3D_vars(datasets[0])
 
     drop_data_vars = set(list(datasets[0].data_vars)) - set(var_list)
 
@@ -39,10 +44,59 @@ def open_mfdataset(files, var_list=None, surf_only=False, **kwargs):
         {"nav_lat": "latitude", "nav_lon": "longitude", "time_counter": "time", "bottom_top": "z"}
     )
 
+    if not surf_only:
+        rename_dict = {}
+        if "temp" in xrds.variables:
+            rename_dict["temp"] = "temperature_k"
+        if "pres" in xrds.variables:
+            rename_dict["pres"] = "pres_pa_mid"
+        if "thlay" in xrds.variables:
+            rename_dict["thlay"] = "dz_m"
+        xrds = xrds.rename(rename_dict)
+
     if surf_only:
         xrds = xrds.isel(z=0).expand_dims("z", axis=1)
 
     xrds = xrds.reset_coords()
     xrds = xrds.set_coords(["latitude", "longitude"])
+    z = list(range(len(xrds["z"].values)))
+    xrds["z"] = (('z',), z)
 
     return xrds
+
+
+def list_met_3D_vars(dataset):
+    """List the 3D met variables
+
+    Parameters
+    ----------
+    dataset : xr.Dataset
+        Dataset containing the variables
+
+    Returns
+    -------
+    list[str]
+        List containing the 3D vars
+    """
+
+    all_vars = set(list(dataset.data_vars))
+
+    vars_3D = []
+
+    if "thlay" in all_vars:
+        vars_3D.append("thlay")
+    else:
+        warnings.warn(
+                "thlay is not a variable. Currently, no other method to calculate the "
+                "the layer thickness is implemented."
+        )
+    if "temp" in all_vars:
+        vars_3D.append("temp")
+    else:
+        warnings.warn("3D temperature not available")
+    if "pres" in all_vars:
+        vars_3D.append("pres")
+    else:
+        warnings.warn("currently adding the pressure requires the pres variable. "
+        "We might implement using the hybrid coordinates in the future")
+    return vars_3D
